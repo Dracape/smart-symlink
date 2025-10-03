9c953eaff3bc8298ed212e3297a136facc051356
Dracape <Dracape@outlook.com>
2025-10-03 20:19:33 +0530

    Add VERBOSE in new Variable Help section

--- scr/lib/main.fish
+++ scr/lib/main.fish
@@ -19,12 +19,19 @@ function smart-symlink --description 'Recursively symlinks a source directory to
 		set_color --bold --underline; echo -n 'Usage:'; set_color normal; echo ' '(status current-function)' [OPTION] SOURCE_DIR TARGET'\n
 		set_color --bold --underline; echo 'Arguments:'; set_color normal; echo \t'<paths>…'\n
 
-		set_color --bold --underline; echo 'Options:'; set_color normal
-		set_color --bold; echo -n -- '  -h'; set_color normal; echo -n ', '; set_color --bold; echo -- '--help'; set_color normal
-		echo \t'Print help'
-		set_color --bold; echo -n -- '  -v'; set_color normal; echo -n ', '; set_color --bold; echo -- '--verbose'; set_color normal
-		echo -n \t'explicitly state what is being done'\n\t'[Env: '; set_color --italics; echo -n 'VERBOSE'; set_color normal; echo ']' 
+		set_color --bold --underline; echo 'Options:'
+		set_color --bold normal; echo -n -- '  -h'; set_color normal; echo -n ', '; set_color --bold; echo -- '--help'
+		set_color normal; echo \t'Print help'
+		set_color --bold; echo -n -- '  -v'; set_color normal; echo -n ', '; set_color --bold; echo -- '--verbose'
+		set_color normal; echo -n \t'Show more information'\n\t'[Variable: '; set_color --italics; echo -n 'VERBOSE'; set_color normal; echo ']' 
 
+		if set -q VERBOSE
+			set_color --bold --underline; echo \n'Variables:'
+			set_color --bold normal; echo '  VERBOSE'
+			set_color normal; echo \t'Show more information'
+			set_color normal; echo -n \t'[Switch: '; set_color --italics; echo -n -- '-v';set_color normal; echo -n ', ';set_color --italics ; echo -n -- '--verbose' ; set_color normal; echo ']'
+		end
+		
 		return 0
 	end
 
