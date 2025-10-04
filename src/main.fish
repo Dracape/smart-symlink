9a93a2c4aab181932fe0a2045427640dee7357c4
Dracape <Dracape@outlook.com>
2025-10-04 17:15:11 +0530

    indent

--- src/main.fish
+++ src/main.fish
@@ -5,6 +5,10 @@
 # 	◦ Create functions for commands to automatically set the permissions to the directory's ownership (using stat -c %[U|G])
 # • Allow interactive use (without force or interactive option; determine what to use on the basis of `status is-interactive`)
 # • Make variables for customization
+
+
+
+
 # Behaviour setting
 ## Variables
 ### Output prefix
