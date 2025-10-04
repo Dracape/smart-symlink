a256731aaf573c48f579c1b1b547e8c6d14694f7
Dracape <Dracape@outlook.com>
2025-10-04 18:21:52 +0530

    Remove hardcode (function exists)

--- src/lib/operate/sub/case/non-recursive/main.fish
+++ src/lib/operate/sub/case/non-recursive/main.fish
@@ -5,15 +5,6 @@ function _smart-symlink_operate_case_non-recursive --description 'Simple, Non-re
 	"$function_name"_verify-source-is-dir # Verify source is a directory
 	"$function_name"_link-entire-on-missing-target # If target doesn't exist, simply create a symlink to the source and exit
 
-	if ! test -e "$target_dir"
-		if set -q VERBOSE # Verbosity announcement
-			echo {$output_prefix}'Target Does not exist: "'{$target_dir}\"
-		end
-
-		ln {$VERBOSE} -s "$source_dir" "$target_dir"
-		exit 0
-	end
-
 	# If target is not a directory, it's a conflict. Overwrite it as a symlink.
 	if ! test -d "$target_dir"
 		echo {$output_prefix}'Warning: Target "'{$target_dir}'" is not a directory'\t'Replacing with symlink' 1>&2
